c7bf9a6872f554471f50c2381f20096cf6195288
William Wingate <wtwingate@gmail.com>
2025-06-06 14:12:37 -0400

    Allow JSON responses for index and show actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -3,9 +3,18 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
   end
 
   def new
